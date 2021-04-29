c8b9030e548aaf77515dacee4942babcf20a1d21
ssisaias <ssisaias@users.noreply.github.com>
2021-04-28 21:38:00 -0300

    Switch to cqrs in controllers

--- API/Controllers/Activitiescontroller.cs
+++ API/Controllers/Activitiescontroller.cs
@@ -1,30 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+
 
 namespace API.Controllers
 {
     public class Activitiescontroller : BaseApiController
     {
-        private readonly DataContext _context;
-        public Activitiescontroller(DataContext context)
-        {
-            this._context = context;
-        }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> getActivities(){
-            return await _context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> getActivities()
+        {
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //activites//id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }

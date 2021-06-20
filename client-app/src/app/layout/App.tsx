347712d147c777a188e644a06aaac6e229dfa9f7
ssisaias <ssisaias@users.noreply.github.com>
2021-06-19 22:26:19 -0300

    Header navbar and design updates

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,16 +14,18 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
-      <List>
-      {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-      </List>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <Container style={{marginTop: '7em'}}>
+        <List>
+        {activities.map((activity) => (
+              <List.Item key={activity.id}>
+                {activity.title}
+              </List.Item>
+            ))}
+        </List>
+      </Container>
+    </>
   );
 }
 

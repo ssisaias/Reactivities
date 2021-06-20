829469c3c45e3c4f17983fbaa7af4544ddf80096
ssisaias <ssisaias@users.noreply.github.com>
2021-06-20 14:45:26 -0300

    activityDashboard and list component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/activityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -17,13 +18,7 @@ function App() {
     <>
       <NavBar></NavBar>
       <Container style={{marginTop: '7em'}}>
-        <List>
-        {activities.map((activity) => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-            ))}
-        </List>
+        <ActivityDashboard activities={activities}></ActivityDashboard>
       </Container>
     </>
   );

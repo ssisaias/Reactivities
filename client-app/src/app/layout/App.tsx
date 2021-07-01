98e746c7b828b56f4795932a2d3c527ea971d446
ssisaias <ssisaias@users.noreply.github.com>
2021-06-30 22:00:37 -0300

    Client code error handling
    Added mock controler on client and back end
    Added test error (for the mock controller) in client
    Client react-toast lib

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -7,12 +7,15 @@ import HomePage from '../../features/home/HomePage';
 import ActivitiyForm from '../../features/activities/form/ActivitiyForm';
 import activityDashboard from '../../features/activities/dashboard/activityDashboard';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const location = useLocation();
 
   return (
     <>
+      <ToastContainer position='bottom-right'></ToastContainer>
       <Route exact path='/' component={HomePage} />
       <Route path={`/(.+)`} render={()=>(
         <>
@@ -21,6 +24,7 @@ function App() {
             <Route exact path='/activities' component={activityDashboard} />
             <Route path='/activities/:id' component={ActivityDetails} />
             <Route path={['/createActivity', '/manage/:id']} key={location.key} component={ActivitiyForm} />
+            <Route path='/errors' component={TestErrors}></Route>
           </Container>
         </>
       )}/>

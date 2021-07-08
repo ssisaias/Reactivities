83489eff0768d1f63685f0aa8f6b8e8d0fb925fb
ssisaias <ssisaias@users.noreply.github.com>
2021-07-07 22:57:19 -0300

    Store the jwt token when logging-in

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -3,11 +3,23 @@ import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
   error: ServerError | null = null;
+  token: string | null = null;
+  appLoaded= false;
+
   constructor() {
     makeAutoObservable(this);
   }
 
   setServerError = (error: ServerError) => {
     this.error = error;
+  }
+
+  setToken = (token: string | null) => {
+    if (token) window.localStorage.setItem('jwt', token);
+    this.token = token;
+  }
+
+  setAppLoaded = () => {
+    this.appLoaded = true;
   }
 }

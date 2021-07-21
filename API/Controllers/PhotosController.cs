c4a0136193cb91ea6340bea25d5ee71c39a82f37
ssisaias <ssisaias@users.noreply.github.com>
2021-07-21 09:51:16 -0300

    Adds delete Photo feature

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -10,5 +10,10 @@ namespace API.Controllers
         public async Task<IActionResult> Add([FromForm] Add.Command command) {
           return HandleResult(await Mediator.Send(command));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id) {
+          return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
+        }
     }
 }

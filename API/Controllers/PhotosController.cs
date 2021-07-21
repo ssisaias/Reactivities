1be511d2ab9e4ac71b61f65c441b24b98dac6704
ssisaias <ssisaias@users.noreply.github.com>
2021-07-21 10:29:09 -0300

    Set main profile picture

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -15,5 +15,10 @@ namespace API.Controllers
         public async Task<IActionResult> Delete(string id) {
           return HandleResult(await Mediator.Send(new Delete.Command{Id = id}));
         }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain(string id) {
+          return HandleResult(await Mediator.Send(new SetMain.Command{Id = id}));
+        }
     }
 }

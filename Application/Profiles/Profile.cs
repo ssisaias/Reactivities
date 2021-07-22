be511b715bdf43d944a39677a29354f08972b4ff
ssisaias <ssisaias@users.noreply.github.com>
2021-07-22 08:49:53 -0300

    Return user profiles with Main image and photo collection

--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -1,3 +1,6 @@
+using Domain;
+using System.Collections.Generic;
+
 namespace Application.Profiles
 {
     public class Profile
@@ -6,5 +9,6 @@ namespace Application.Profiles
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
